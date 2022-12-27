1eb81945c5c80433a750d5c767cf2a0a7297f1e5
Renboou <94653387+Renboou@users.noreply.github.com>
2022-12-26 19:10:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function startClassification(){
+navigator.mediaDevices.getUserMedia({ audio: true });
+classifier =  ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/5HIMbPUBbc/model.json', modelReady);
+}
+
+function modelReady(){
+classifier.classify(gotResults);
+}
+
+
+
+
+
+
+
+
+
+
+
